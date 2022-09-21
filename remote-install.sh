329fa205ad5796c1c31c96236365e10fbe10d35c
Drew Ayling <drew.ayling@livongo.com>
2022-09-21 16:14:26 -0600

    feat: add remote-install

---
+++ remote-install.sh
@@ -0,0 +1,5 @@
+tar_url=`curl -H "Accept: application/vnd.github+json" https://api.github.com/repos/dsayling/outlook-sound-sync/releases/latest | python3 -c "import sys, json; print(json.load(sys.stdin)['tarball_url'])"`
+mkdir -p /tmp/oss/extracted
+wget "$tar_url" -O /tmp/oss/outlook-sound-sync.tar.gz
+tar -xvf /tmp/oss/outlook-sound-sync.tar.gz --strip-components 1 -C /tmp/oss/extracted
+sh -c "/tmp/oss/extracted/src/install-outlook-sync.sh"
--- src/install-outlook-sync.sh
+++ src/install-outlook-sync.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env sh
+CUR_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # prompt the user for the location of the sounds files
 echo "Starting setup for outlook-sounds-sync"
 
-# read -p "Sounds Path [$HOME/.outlook-sounds]:" DEFAULT_PATH
+read -p "Sounds Path [$HOME/.outlook-sounds]:" DEFAULT_PATH
 
-# if [ -z "$DEFAULT_PATH" ]
-# then
-echo "Using default sounds path"
-DEFAULT_PATH="$HOME/.outlook-sounds"
-# fi
-# make the directory and mv the soundsync script
-CUR_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+if [ -z "$DEFAULT_PATH" ]
+then
+	echo "Using default sounds path"
+	DEFAULT_PATH="$HOME/.outlook-sounds"
+fi
 
+# make the sounds directory and mv the soundsync script
 mkdir -p "$DEFAULT_PATH"
 cp "$CUR_DIR/soundsync.sh" "$DEFAULT_PATH"
 SCRIPT_PATH="$DEFAULT_PATH/soundsync.sh"
 
 # update the plist file with the user information
-sed -i.bak "s@{SOUNDS_PATH}@$SCRIPT_PATH@" com.outlook.sounds.sync.plist
+sed -i.bak "s@{SOUNDS_PATH}@$SCRIPT_PATH@" "$CUR_DIR/com.outlook.sounds.sync.plist"
 
 # move plist file to /Library/LaunchDaemons as root
 sudo mv com.outlook.sounds.sync.plist /Library/LaunchDaemons
