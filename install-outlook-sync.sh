dc8db86453e901b71855386e84291d1da2396adf
Drew Ayling <drew.ayling@livongo.com>
2022-09-21 13:44:58 -0600

    initial commit of scripts

---
+++ install-outlook-sync.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+# prompt the user for the location of the sounds files
+echo "Starting setup for outlook-sounds-sync"
+
+read -p "Sounds Path [$HOME/.outlook-sounds]:" DEFAULT_PATH
+
+if [ -z "$DEFAULT_PATH" ]
+then
+	echo "Using default sounds path"
+	DEFAULT_PATH="$HOME/.outlook-sounds"
+fi
+# make the directory and mv the soundsync script
+
+mkdir -p "$DEFAULT_PATH"
+cp ./soundsync.sh "$DEFAULT_PATH"
+SCRIPT_PATH="$DEFAULT_PATH/soundsync.sh"
+
+# update the plist file with the user information
+sed -i.bak "s@{SOUNDS_PATH}@$SCRIPT_PATH@" com.outlook.sounds.sync.plist
+
+# move plist file to /Library/LaunchDaemons as root
+sudo mv com.outlook.sounds.sync.plist /Library/LaunchDaemons
+
+# load the new plist
+sudo launchctl load /Library/LaunchDaemons/com.outlook.sounds.sync.plist
+
---
+++ soundsync.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+outlookPath="/Applications/Microsoft Outlook.app/Contents/Frameworks/OutlookCore.framework/Versions/Current/Resources"
+# when you set this up as a root launchdaemon, you'll need to put the full path
+# or to a root path
+default_soundPath=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+allSounds=("goodbye.wav" "mailsent.wav" "nomail.wav" "welcome.wav" "mailerror.wav" "newmail.wav" "reminder.wav")
+
+for sound in ${allSounds[@]}; do
+    # check the file exists
+    echo "Checking for $default_soundPath/$sound"
+    if [ -f "$default_soundPath/$sound" ]; then
+        echo "Found $sound"
+        if [ `cmp -s "$default_soundPath/$sound" "$outlookPath/$sound"` ]; then
+            sudo cp "$default_soundPath/$sound" "$outlookPath/$sound"
+            echo "Copied $sound"
+        else
+            echo "$sound File are still the same"
+        fi
+    else
+        echo "didnt find $sound"
+    fi
+    continue
+done
+
